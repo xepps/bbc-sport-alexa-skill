d257ab4042c2320db7f58be35cc13ce75745aa9e
Kristian Epps <kristian.epps@bbc.co.uk>
2017-04-12 11:27:23 +0100

    another neat refactor with common functions

--- index.js
+++ index.js
@@ -1,75 +1,19 @@
 'use strict';
 var Alexa = require('alexa-sdk');
-var request = require('./lib/request');
 var moment = require('moment');
+
+var request = require('./lib/request');
+var helper = require('./lib/helper');
 
 var APP_ID = 'amzn1.ask.skill.c777bb28-a73a-4428-94d7-b2eee73864c5';
 var SKILL_NAME = 'BBC Sport';
 
-function getTeam(team) {
-    return {
-        'afc bournemouth': 'afc-bournemouth',
-        'arsenal': 'arsenal',
-        'burnley': 'burnley',
-        'chelsea': 'chelsea',
-        'crystal palace': 'crystal-palace',
-        'everton': 'everton',
-        'hull city': 'hull-city',
-        'leicester city': 'leicester-city',
-        'liverpool': 'liverpool',
-        'manchester city': 'manchester-city',
-        'manchester united': 'manchester-united',
-        'middlesbrough': 'middlesbrough',
-        'southampton': 'southampton',
-        'stoke city': 'stoke-city',
-        'sunderland': 'sunderland',
-        'swansea city': 'swansea-city',
-        'tottenham hotspur': 'tottenham-hotspur',
-        'watford': 'watford',
-        'west bromwich albion': 'west-bromwich-albion',
-        'west ham united': 'west-ham-united'
-    }[team.toLowerCase()] || null;
-}
-
-function getMyTeam(team, homeTeam, awayTeam) {
-    return homeTeam.name.first.toLowerCase() === team.toLowerCase()
-        ? homeTeam
-        : awayTeam;
-}
-
-function getOppositionTo(team, homeTeam, awayTeam) {
-    return homeTeam.name.first.toLowerCase() === team.toLowerCase()
-        ? awayTeam
-        : homeTeam;
-}
-
-function getHomeOrAway(team, homeTeam, awayTeam) {
-    return homeTeam.name.first === team
-        ? ' at home'
-        : ', away,'; // Commas are required in order for Alexa to pronounce team names correctly!
-}
-
-function verifyTeamName(event, onValid) {
-    var team = event.request.intent.slots.Team.value;
-    var teamSlug = getTeam(team);
-
-    if (teamSlug === null) {
-        this.emit(':tellWithCard', 'Please give me the name of a premier league team.', team, SKILL_NAME, 'Unknown');
-        return;
-    }
-
-    onValid({
-        name: team,
-        slug: teamSlug
-    });
-}
-
 function getNextFixture(team, fixtures) {
     if (fixtures.rounds.length) {
         var event = fixtures.rounds[0].events[0];
-        var myTeam = getMyTeam(team, event.homeTeam, event.awayTeam).name.first;
-        var opposingTeam = getOppositionTo(team, event.homeTeam, event.awayTeam).name.first;
-        var homeOrAway = getHomeOrAway(myTeam, event.homeTeam, event.awayTeam);
+        var myTeam = helper.getMyTeam(team, event.homeTeam, event.awayTeam).name.first;
+        var opposingTeam = helper.getOppositionTo(team, event.homeTeam, event.awayTeam).name.first;
+        var homeOrAway = helper.getHomeOrAway(myTeam, event.homeTeam, event.awayTeam);
         var fromNow = moment(event.startTime).fromNow();
 
         return myTeam + ' are playing ' + opposingTeam + homeOrAway + ' ' + fromNow;
@@ -93,7 +37,7 @@ var handlers = {
         this.emit('NextFixture');
     },
     'NextFixture': function () {
-        verifyTeamName(this.event, function onValid(team) {
+        helper.verifyTeamName(this.event, function onValid(team) {
             request(team.slug, function onError() {
                 this.emit(':tellWithCard', 'Response timed out. Please try again.', SKILL_NAME, 'Timed out', team.name);
             }, function onSuccess(events) {
